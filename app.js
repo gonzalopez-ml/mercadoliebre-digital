2015bf720f397f86e370a9c9e9572964793e1111
gonzalopez-ml <gonzalo.glopez@mercadolibre.com>
2021-11-15 19:12:55 -0300

    ase agregan ejercicios

--- app.js
+++ app.js
@@ -6,8 +6,20 @@ const port = 3000;
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath)); //le dice a express que public es un recurso estatico
 
-app.get("/home", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.get("/register", function (req, res) {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
+app.get("/login", function (req, res) {
+  res.sendFile(path.join(__dirname, "/views/login.html"));
+});
+
+app.post("/login", function (req, res) {
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || port, () => {
